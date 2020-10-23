eff112c99a6de815141f15308782a34f2af568ed
kobakakzu0429 <kobakazu10@gmail.com>
2020-10-23 16:47:43 +0900

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import App from "next/app";
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }: any) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return <Component {...pageProps} />;
+  }
+}
