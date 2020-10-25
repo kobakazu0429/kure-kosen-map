37d3498e993d15367b2ac26b51a0d4abbf831de5
kobakakzu0429 <kobakazu10@gmail.com>
2020-10-25 10:48:41 +0900

    add TextFiled and SeachField

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { SideBarLayout } from "@/layouts/SideBar";
 import { KureKosenMap } from "@/components/KureKosenMap";
+import { SearchField } from "@/components/TextField";
 
 const Nav = () => {
   return (
     <nav className="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
+      <SearchField />
       <a
         className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
         href="#"
@@ -32,53 +34,6 @@ const Nav = () => {
     </nav>
   );
 };
-
-/* <div onClick={close} className="relative">
-  <button
-    onClick={toggle}
-    className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-  >
-    <span>Dropdown</span>
-    <svg
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      className={`${
-        open ? "rotate-180" : "rotate-0"
-      } inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1`}
-    >
-      <path
-        fillRule="evenodd"
-        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-        clipRule="evenodd"
-      ></path>
-    </svg>
-  </button>
-  <div
-    x-show="open"
-    className="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg"
-  >
-    <div className="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-      <a
-        className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-        href="#"
-      >
-        Link #1
-      </a>
-      <a
-        className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-        href="#"
-      >
-        Link #2
-      </a>
-      <a
-        className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-        href="#"
-      >
-        Link #3
-      </a>
-    </div>
-  </div>
-</div> */
 
 export default function TopPage() {
   return <SideBarLayout sideComp={<Nav />} mainComp={<KureKosenMap />} />;
