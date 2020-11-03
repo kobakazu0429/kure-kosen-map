9a2bcb5cc057fdb91e9e5cd7a5293436cf219088
kobakakzu0429 <kobakazu10@gmail.com>
2020-11-03 11:10:36 +0900

    include src/** with tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./src/**/*.tsx"],
   theme: {},
   variants: {},
   plugins: [],
