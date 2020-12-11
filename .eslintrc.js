40aa82b5a3b453b01fd22dd084c97724102a973b
kobakakzu0429 <kobakazu10@gmail.com>
2020-12-11 22:06:10 +0900

    ban-ts-comment:off, no-non-null-assertion:warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: "@kobakazu0429/eslint-config-typescript-react",
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-non-null-assertion": "warn",
+  },
 };
