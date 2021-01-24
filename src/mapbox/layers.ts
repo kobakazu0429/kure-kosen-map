ada57da02426377a4f79b407ab1f56da5adb08c8
kobakakzu0429 <kobakazu10@gmail.com>
2021-01-25 00:24:49 +0900

    add pre master data

--- src/mapbox/layers.ts
+++ src/mapbox/layers.ts
@@ -1,22 +1,76 @@
 import { FeatureCollection } from "geojson";
 import { MapWrapper } from "./hooks";
-import kurekosenGeojson from "../geojson/kurekosen.json";
+// import kurekosenGeojson from "../geojson/edited-complete13.json";
 
-export const kureKosen: mapboxgl.Layer = {
-  id: "kurekosen",
-  type: "fill",
-  source: {
-    type: "geojson",
-    data: kurekosenGeojson as FeatureCollection,
+import 外形 from "../geojson/edited-外形.json";
+import 道路 from "../geojson/edited-道路.json";
+import 建物 from "../geojson/edited-建物.json";
+import 駐車場 from "../geojson/edited-駐車場.json";
+import 駐輪場 from "../geojson/edited-駐輪場.json";
+import オーバーレイ from "../geojson/edited-オーバーレイ.json";
+
+// @ts-expect-error
+const knctLayers: mapboxgl.Layer[] = [
+  { id: "外形", data: 外形, "fill-color": "#e7e5d9" },
+  {
+    id: "道路",
+    data: 道路,
+    "fill-color": "#fff",
+    // "fill-outline-color": "#555",
   },
-  paint: {
-    "fill-color": "#088",
-    "fill-opacity": 0.6,
+  { id: "建物", data: 建物, "fill-color": "#dcd3bf" },
+  {
+    id: "駐車場",
+    data: 駐車場,
+    "fill-color": "#e7e5d9",
+    "fill-outline-color": "#535353",
   },
-  layout: {
-    visibility: "visible",
+  { id: "駐輪場", data: 駐輪場, "fill-outline-color": "#535353" },
+  {
+    id: "オーバーレイ",
+    data: オーバーレイ,
+    "fill-color": "#e7e5d9",
+    "fill-outline-color": "#535353",
   },
-};
+].map((v) => {
+  const s = {
+    id: v.id,
+    type: "fill",
+    source: {
+      type: "geojson",
+      data: v.data as FeatureCollection,
+    },
+    paint: {
+      "fill-opacity": 1.0,
+    },
+    layout: {
+      visibility: "visible",
+    },
+  };
+  // @ts-expect-error
+  if (v["fill-color"]) s.paint["fill-color"] = v["fill-color"];
+  if (v["fill-outline-color"])
+    // @ts-expect-error
+    s.paint["fill-outline-color"] = v["fill-outline-color"];
+
+  return s;
+});
+
+// export const kureKosen: mapboxgl.Layer = {
+//   id: "kurekosen",
+//   type: "fill",
+//   source: {
+//     type: "geojson",
+//     data: 外形 as FeatureCollection,
+//   },
+//   paint: {
+//     "fill-color": "#e7e5d9",
+//     "fill-opacity": 1.0,
+//   },
+//   layout: {
+//     visibility: "visible",
+//   },
+// };
 
 export interface PanoramFeatureProperties {
   placename: string;
@@ -49,9 +103,9 @@ export const panorama: mapboxgl.Layer = {
   },
 };
 
-const layers = [kureKosen, panorama];
-export const popuppableLayerIds = [kureKosen].map(({ id }) => id);
-export const toggleableLayerIds = [kureKosen].map(({ id }) => id);
+const layers = [...knctLayers, panorama];
+export const popuppableLayerIds = [].map(({ id }) => id);
+export const toggleableLayerIds = [].map(({ id }) => id);
 
 export function registerLayers(map: MapWrapper) {
   map.initialize.on((mapbox) => {
